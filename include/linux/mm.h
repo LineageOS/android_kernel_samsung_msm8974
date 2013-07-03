2da9c867ff8a91b235b0c5aa6e2a522ce6eee0b5
Andrew Morton <akpm@linux-foundation.org>
2013-07-03 15:02:11 -0700

    UPSTREAM: include/linux/mm.h: add PAGE_ALIGNED() helper
    To test whether an address is aligned to PAGE_SIZE.
    
    Cc: HATAYAMA Daisuke <d.hatayama@jp.fujitsu.com>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>,
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 0fa73b86ef0797ca4fde5334117ca0b330f08030)
    
    Bug: 36007193
    Change-Id: I7e912bb0dbd8c9737fb13c5b48acb54ee39dd5fc
    CVE-2017-13164
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -63,6 +63,9 @@ extern int mmap_rnd_compat_bits __read_mostly;
 /* to align the pointer to the (next) page boundary */
 #define PAGE_ALIGN(addr) ALIGN(addr, PAGE_SIZE)
 
+/* test whether an address (unsigned long or pointer) is aligned to PAGE_SIZE */
+#define PAGE_ALIGNED(addr)	IS_ALIGNED((unsigned long)addr, PAGE_SIZE)
+
 /*
  * Linux kernel virtual memory manager primitives.
  * The idea being to have a "virtual" mm in the same way
