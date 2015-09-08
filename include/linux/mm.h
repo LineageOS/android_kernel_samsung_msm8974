f99851f8ed154dfa5d4420bc805bc3e8bdef9d87
Oleg Nesterov <oleg@redhat.com>
2015-09-08 14:58:28 -0700

    mm: introduce vma_is_anonymous(vma) helper
    commit b5330628546616af14ff23075fbf8d4ad91f6e25 upstream.
    
    special_mapping_fault() is absolutely broken.  It seems it was always
    wrong, but this didn't matter until vdso/vvar started to use more than
    one page.
    
    And after this change vma_is_anonymous() becomes really trivial, it
    simply checks vm_ops == NULL.  However, I do think the helper makes
    sense.  There are a lot of ->vm_ops != NULL checks, the helper makes the
    caller's code more understandable (self-documented) and this is more
    grep-friendly.
    
    This patch (of 3):
    
    Preparation.  Add the new simple helper, vma_is_anonymous(vma), and change
    handle_pte_fault() to use it.  It will have more users.
    
    The name is not accurate, say a hpet_mmap()'ed vma is not anonymous.
    Perhaps it should be named vma_has_fault() instead.  But it matches the
    logic in mmap.c/memory.c (see next changes).  "True" just means that a
    page fault will use do_anonymous_page().
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Pavel Emelyanov <xemul@parallels.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [bwh: Backported to 3.16 as dependency of "mm/mincore.c: make mincore() more
     conservative"; adjusted context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    CVE-2019-5489
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Change-Id: I179b12b1f76810b4ca08480c03b4b0e0985fe45e

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1066,6 +1066,11 @@ int get_cmdline(struct task_struct *task, char *buffer, int buflen);
 extern pid_t
 vm_is_stack(struct task_struct *task, struct vm_area_struct *vma, int in_group);
 
+static inline bool vma_is_anonymous(struct vm_area_struct *vma)
+{
+	return !vma->vm_ops;
+}
+
 extern unsigned long move_page_tables(struct vm_area_struct *vma,
 		unsigned long old_addr, struct vm_area_struct *new_vma,
 		unsigned long new_addr, unsigned long len);
