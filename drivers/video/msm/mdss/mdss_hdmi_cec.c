8228a544782d99355dd8f7ad19d477b9ec9f6e74
zhaoyuan <yzhao@codeaurora.org>
2017-02-20 13:42:20 +0800

    msm: mdss: hdmi: check up-bound of CEC frame size
    the spec says the frame size will not be greater than
    14, but this have a security hole when somebody sends
    a message with a size greater than 14. So need check
    up-boud of the CEC frame size.
    
    Bug: 64438726
    Change-Id: I743208badc5e77ae911cfb2d102f758d4843138f
    Signed-off-by: zhaoyuan <yzhao@codeaurora.org>
    CVE-2017-9719
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/video/msm/mdss/mdss_hdmi_cec.c
+++ drivers/video/msm/mdss/mdss_hdmi_cec.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -22,7 +22,10 @@
 
 /* Reference: HDMI 1.4a Specification section 7.1 */
 #define RETRANSMIT_MAX_NUM	5
-#define MAX_OPERAND_SIZE	15
+#define MAX_OPERAND_SIZE	14
+
+/* total size:  HEADER block (1) + opcode block (1) + operands (14) */
+#define MAX_CEC_FRAME_SIZE      (MAX_OPERAND_SIZE + 2)
 
 /*
  * Ref. HDMI 1.4a: Supplement-1 CEC Section 6, 7
@@ -417,7 +420,7 @@ static void hdmi_cec_msg_recv(struct work_struct *work)
 		msg_node->msg.sender_id, msg_node->msg.recvr_id,
 		msg_node->msg.frame_size);
 
-	if (msg_node->msg.frame_size < 1) {
+	if (msg_node->msg.frame_size < 1 || msg_node->msg.frame_size > MAX_CEC_FRAME_SIZE) {
 		DEV_ERR("%s: invalid message (frame length = %d)",
 			__func__, msg_node->msg.frame_size);
 		kfree(msg_node);
@@ -439,7 +442,7 @@ static void hdmi_cec_msg_recv(struct work_struct *work)
 		msg_node->msg.operand[i] = data & 0xFF;
 	}
 
-	for (; i < 14; i++)
+	for (; i < MAX_OPERAND_SIZE; i++)
 		msg_node->msg.operand[i] = 0;
 
 	DEV_DBG("%s: CEC read frame done\n", __func__);
