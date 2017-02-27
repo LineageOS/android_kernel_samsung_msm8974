f4739a2e27d84aba37a1f32f5c3946a9685c0987
LuK1337 <priv.luk@gmail.com>
2017-02-27 13:49:33 +0100

    input: touchscreen: synaptics_rmi: Add FB notifier callback
    Change-Id: I650f3224c94b94d2bdc2c640a642e05e3efd434d
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    (cherry picked from commit eada7d6e149f8a887090ac3a2e094ddd4759913d)

--- drivers/input/touchscreen/synaptics/synaptics_i2c_rmi.c
+++ drivers/input/touchscreen/synaptics/synaptics_i2c_rmi.c
@@ -31,6 +31,9 @@
 #include <linux/of_gpio.h>
 #include <linux/regulator/consumer.h>
 #include <linux/qpnp/pin.h>
+#ifdef CONFIG_FB
+#include <linux/fb.h>
+#endif
 
 #define DRIVER_NAME "synaptics_rmi4_i2c"
 
@@ -5132,6 +5135,12 @@ static void msm_tkey_led_set(struct led_classdev *led_cdev,
 #if defined(CONFIG_FB_MSM8x26_MDSS_CHECK_LCD_CONNECTION)
 extern int get_lcd_attached(void);
 #endif
+
+#ifdef CONFIG_FB
+static int fb_notifier_callback(struct notifier_block *self,
+	unsigned long event, void *data);
+#endif
+
 /**
  * synaptics_rmi4_probe()
  *
@@ -5385,6 +5394,13 @@ err_tsp_reboot:
 /* turn off touch IC, will be turned by InputRedaer */
 	synaptics_rmi4_stop_device(rmi4_data);
 #endif
+
+#ifdef CONFIG_FB
+	rmi4_data->fb_notif.notifier_call = fb_notifier_callback;
+	if (fb_register_client(&rmi4_data->fb_notif))
+		pr_err("%s: could not create fb notifier\n", __func__);
+#endif
+
 	return retval;
 
 #if defined(CONFIG_LEDS_CLASS) && defined(TOUCHKEY_ENABLE)
@@ -5501,6 +5517,10 @@ static int __devexit synaptics_rmi4_remove(struct i2c_client *client)
 	mutex_destroy(&(rmi4_data->rmi4_reset_mutex));
 	mutex_destroy(&(rmi4_data->rmi4_reflash_mutex));
 	mutex_destroy(&(rmi4_data->rmi4_device_mutex));
+
+#ifdef CONFIG_FB
+	fb_unregister_client(&rmi4_data->fb_notif);
+#endif
 
 	kfree(rmi4_data);
 
@@ -5909,6 +5929,36 @@ static int synaptics_rmi4_resume(struct device *dev)
 	}
 
 	mutex_unlock(&rmi4_data->input_dev->mutex);
+
+	return 0;
+}
+#endif
+
+#ifdef CONFIG_FB
+static int fb_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
+{
+	struct fb_event *evdata = data;
+	struct synaptics_rmi4_data *rmi4_data =
+			container_of(self, struct synaptics_rmi4_data, fb_notif);
+
+	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
+		int *blank = evdata->data;
+		switch (*blank) {
+		case FB_BLANK_UNBLANK:
+		case FB_BLANK_NORMAL:
+		case FB_BLANK_VSYNC_SUSPEND:
+		case FB_BLANK_HSYNC_SUSPEND:
+			synaptics_rmi4_resume(&rmi4_data->i2c_client->dev);
+			break;
+		case FB_BLANK_POWERDOWN:
+			synaptics_rmi4_suspend(&rmi4_data->i2c_client->dev);
+			break;
+		default:
+			/* Don't handle what we don't understand */
+			break;
+		}
+	}
 
 	return 0;
 }
