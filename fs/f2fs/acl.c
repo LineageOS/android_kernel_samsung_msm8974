9b43fa1929e4413e66b27bd8ba3784cb3b2a3440
Zhang Shengju <zhangshengju@cmss.chinamobile.com>
2017-06-01 16:50:10 +0800

    f2fs: remove the unnecessary cast for PTR_ERR
    It's not necessary to specify 'int' casting for PTR_ERR.
    
    Signed-off-by: Zhang Shengju <zhangshengju@cmss.chinamobile.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/acl.c
+++ fs/f2fs/acl.c
@@ -234,7 +234,7 @@ static int f2fs_set_acl(struct inode *inode, int type,
 		value = f2fs_acl_to_disk(acl, &size);
 		if (IS_ERR(value)) {
 			clear_inode_flag(inode, FI_ACL_MODE);
-			return (int)PTR_ERR(value);
+			return PTR_ERR(value);
 		}
 	}
 
