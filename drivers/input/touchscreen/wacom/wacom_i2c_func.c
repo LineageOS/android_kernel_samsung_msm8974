5ecbeff0ef309a19fc5d5561f022c9e4a97fc100
Paul Keith <javelinanddart@gmail.com>
2018-03-31 09:59:44 +0200

    wacom: Follow-up from gestures patch
    * Add a macro to convert keycode to gesture bit
    * Use unsigned types for bitmask and keycode
    * Use s16 for x and y coords, matching last_[x/y]
    * Use s64 for dt, which is what ktime_to_ms returns
    
    Change-Id: I1e7be0d1f42f3f96b6f3f47b453190a0ea7819b4

--- drivers/input/touchscreen/wacom/wacom_i2c_func.c
+++ drivers/input/touchscreen/wacom/wacom_i2c_func.c
@@ -907,11 +907,11 @@ void wacom_i2c_softkey(struct wacom_i2c *wac_i2c, s16 key, s16 pressed)
 }
 #endif
 
-static void handle_gestures(int x, int y, ktime_t end, struct wacom_i2c *wac_i2c)
+static void handle_gestures(s16 x, s16 y, ktime_t end, struct wacom_i2c *wac_i2c)
 {
-	int dx = x - wac_i2c->gesture_start_x;
-	int dy = y - wac_i2c->gesture_start_y;
-	int dt = ktime_to_ms(ktime_sub(end, wac_i2c->gesture_start_time));
+	s32 dx = x - wac_i2c->gesture_start_x;
+	s32 dy = y - wac_i2c->gesture_start_y;
+	s64 dt = ktime_to_ms(ktime_sub(end, wac_i2c->gesture_start_time));
 
 	if (abs(dy) > abs(dx)) {
 		if (abs(dy) > MIN_GEST_DIST) {
@@ -1175,9 +1175,9 @@ int wacom_i2c_coord(struct wacom_i2c *wac_i2c)
 						"%s: side off\n",
 						__func__);
 				handle_gestures(x, y, ktime_get(), wac_i2c);
-				if (wac_i2c->gesture_key > -1 &&
+				if (wac_i2c->gesture_key < KEY_MAX &&
 						(wac_i2c->enabled_gestures &
-						(1 << (wac_i2c->gesture_key - 0x2f1)))) {
+						GEST_KEY2BIT(wac_i2c->gesture_key))) {
 					input_report_key(wac_i2c->input_dev,
 							wac_i2c->gesture_key, 1);
 					input_sync(wac_i2c->input_dev);
