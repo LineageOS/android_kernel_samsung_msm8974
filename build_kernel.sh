535e8611324da263b9243e5e5c8a3bf315e5a0e1
Kevin F. Haggerty <haggertk@lineageos.org>
2018-10-15 06:46:49 -0600

    misc: Import SM-G900F kernel source code
    * ro.build.version.security_patch=2017-08-01
    * Samsung Package Version: G900FXXU1CRH1
    
    Change-Id: I91326b90191533a29d9a6adda7032808c0f41ce1
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,6 +6,6 @@ export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 mkdir output
 
 make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+make -j64 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
