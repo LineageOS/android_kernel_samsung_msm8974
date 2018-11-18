1c8d7d5b66f2b4e8de7e46d5d9a0b919d2c243ee
Arne Coucheron <arco68@gmail.com>
2018-11-18 23:57:39 +0100

    net: Add missing LOOPBACK_IFINDEX change in ipv6/route.c
    Missed in "net: Loopback ifindex is constant now" backport.
    
    Change-Id: I7259ac064e9ece4520ff355d55ffd04646d384ce
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -963,7 +963,7 @@ struct dst_entry * ip6_route_output(struct net *net, const struct sock *sk,
 	
 	fl6->flowi6_iif = LOOPBACK_IFINDEX;
 
-	fl6->flowi6_iif = net->loopback_dev->ifindex;
+	fl6->flowi6_iif = LOOPBACK_IFINDEX;
 
 	if ((sk && sk->sk_bound_dev_if) || rt6_need_strict(&fl6->daddr))
 		flags |= RT6_LOOKUP_F_IFACE;
