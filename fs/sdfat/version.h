f2af85cc5125ce562f265cf5edda59f3770af5eb
Kevin F. Haggerty <haggertk@lineageos.org>
2019-01-12 09:10:18 -0700

    fs: sdfat: Update to version 2.1.8
    * Samsung version G960FXXU2CRLI
    
    Change-Id: Ib935f8a5eae8d6145e7b585cc9239caef1d7216b
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- fs/sdfat/version.h
+++ fs/sdfat/version.h
@@ -22,4 +22,4 @@
 /*  PURPOSE : sdFAT File Manager                                        */
 /*                                                                      */
 /************************************************************************/
-#define SDFAT_VERSION	"2.0.6-lineage"
+#define SDFAT_VERSION	"2.1.8-lineage"
