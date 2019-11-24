22265f8133d1334c5ca4ac7d0e9517e68ad4b0e5
Paul Keith <javelinanddart@gmail.com>
2019-11-23 19:48:44 -0600

    drivers: battery: max17050_fuelguage: Migrate to alarmtimer interface
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    Change-Id: I4f995ad71906a23551e7b05c7f3d4b9d4e19ed0a

--- include/linux/battery/fuelgauge/max17050_fuelgauge.h
+++ include/linux/battery/fuelgauge/max17050_fuelgauge.h
@@ -18,7 +18,7 @@
 #ifndef __MAX17050_FUELGAUGE_H
 #define __MAX17050_FUELGAUGE_H __FILE__
 
-#include <linux/android_alarm.h>
+#include <linux/hrtimer.h>
 
 #if defined(CONFIG_MACH_LT03SKT) || defined(CONFIG_MACH_LT03KTT) || \
 	defined(CONFIG_MACH_LT03LGT) || defined(CONFIG_MACH_MONDRIAN)
