ffd1ccdae8bc3547c2eab37f7d38e57335ed7418
Kevin F. Haggerty <haggertk@lineageos.org>
2020-04-28 14:57:35 -0600

    msm: camera_ll: Drop Samsung-added "qctkd" log outputs
    * Proper resolution of a UAF CVE requires nulling clk_ptr[i]
      but this log message requires dereferncing this item. The
      log message is worthless, so ditch it.
    
    Change-Id: I07d21ff30ef4d2908f2fd1036921773ded32ef24
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/media/platform/msm/camera_ll/sensor/io/msm_camera_io_util.c
+++ drivers/media/platform/msm/camera_ll/sensor/io/msm_camera_io_util.c
@@ -125,11 +125,7 @@ int msm_cam_clk_enable(struct device *dev, struct msm_cam_clk_info *clk_info,
 {
 	int i;
 	int rc = 0;
-	int qctkd = 0;
 	long clk_rate;
-
-	if (num_clk == 8) //CPP use case
-		qctkd = 1;
 
 	if (enable) {
 		for (i = 0; i < num_clk; i++) {
@@ -187,7 +183,6 @@ int msm_cam_clk_enable(struct device *dev, struct msm_cam_clk_info *clk_info,
 				usleep_range(clk_info[i].delay * 1000,
 					(clk_info[i].delay * 1000) + 1000);
 			}
-			if (qctkd) printk (KERN_ERR "QCTKD: %s[%d:%d] Enable \n", clk_info[i].clk_name, clk_ptr[i]->prepare_count, clk_ptr[i]->count);
 		}
 	} else {
 		for (i = num_clk - 1; i >= 0; i--) {
@@ -198,7 +193,6 @@ int msm_cam_clk_enable(struct device *dev, struct msm_cam_clk_info *clk_info,
 				clk_unprepare(clk_ptr[i]);
 				clk_put(clk_ptr[i]);
 			}
-			if (qctkd) printk (KERN_ERR "QCTKD: %s[%d:%d] Disable\n", clk_info[i].clk_name, clk_ptr[i]->prepare_count, clk_ptr[i]->count);
 		}
 	}
 	return rc;
